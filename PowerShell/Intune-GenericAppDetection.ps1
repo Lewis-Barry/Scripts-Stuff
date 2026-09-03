913a04728f640073e57a74379a4cf3d50cdfcf27
Lewis Barry <41471929+Lewis-Barry@users.noreply.github.com>
2026-09-03 12:23:19 +0100

    Refactor app detection script variable names

--- PowerShell/Intune-GenericAppDetection.ps1
+++ PowerShell/Intune-GenericAppDetection.ps1
@@ -1,7 +1,10 @@
-$n="AppName"
-$s='HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'|%{Get-ItemProperty $_ -EA 0}|?{$_.DisplayName -like "*$n*"}|select -f 1
-$u='HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'|%{Get-ItemProperty $_ -EA 0}|?{$_.DisplayName -like "*$n*"}|select -f 1
-$d="$env:LOCALAPPDATA\Programs\*","$env:LOCALAPPDATA\*","$env:APPDATA\*"|%{Get-Item $_ -EA 0}|?{$_.Name -like "*$n*"}|select -f 1
+$n="APPNAME"
+$p="*$n*"
+$k='\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$s=$k|%{gp "HKLM:$_" -EA 0}|?{$_.DisplayName -like $p}|select -f 1
+$u=$k|%{gp "HKCU:$_" -EA 0}|?{$_.DisplayName -like $p}|select -f 1
+$l=$env:LOCALAPPDATA
+$d="$l\Programs\*","$l\*","$env:APPDATA\*"|%{gi $_ -EA 0}|?{$_.Name -like $p}|select -f 1
 if($s){Write-Host "$($s.DisplayName) is installed (system-wide).";exit 0}
 elseif($u){Write-Host "$($u.DisplayName) is installed (per-user).";exit 2}
 elseif($d){Write-Host "$($d.Name) found in $($d.FullName) (per-user).";exit 2}
